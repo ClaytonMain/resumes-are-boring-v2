4fb83689bf950368bfeeb8a1f739067982c96952
ClaytonMain <main.clayton@gmail.com>
2026-02-06 17:08:13 -0700

    Checkpoint

--- src/pages/enter/EnterHtml.tsx
+++ src/pages/enter/EnterHtml.tsx
@@ -8,7 +8,7 @@ export default function EnterHtml() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <motion.div className="flex h-10/12 w-full flex-col rounded-xl border px-12 py-12 sm:w-5/12">
+      <motion.div className="flex h-10/12 w-full flex-col rounded-xl border px-12 pt-12 pb-8 sm:w-5/12">
         <motion.h1
           className="my-0 py-0 text-7xl font-bold tracking-tighter"
           initial={{ opacity: 0, y: 20 }}
@@ -57,8 +57,13 @@ export default function EnterHtml() {
         >
           BORING.
         </motion.h1>
-        <motion.div className="mt-auto flex justify-end text-2xl tracking-tighter italic">
-          click anywhere to enter
+        <motion.div
+          className="mt-auto flex justify-end text-2xl font-thin tracking-tight"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.75, delay: 5 } }}
+          exit={{ opacity: 0, transition: { duration: 0.25, delay: 0.75 } }}
+        >
+          click to continue
         </motion.div>
       </motion.div>
     </motion.div>
