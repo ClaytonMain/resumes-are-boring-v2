7486302ab223ad471dc51089752c0161bb803b4b
Tycho-Euler <main.clayton@gmail.com>
2026-03-19 17:29:01 -0600

    Checkpoint

--- src/components/ContactDisplay.tsx
+++ src/components/ContactDisplay.tsx
@@ -1,10 +1,7 @@
-import {
-  Decal,
-  Icosahedron,
-  MeshTransmissionMaterial,
-  useTexture,
-} from "@react-three/drei";
+import { Decal, Icosahedron, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
+import { color } from "motion/react";
 import { useRef } from "react";
 import * as THREE from "three";
 import useAppStore from "../stores/useAppStore";
@@ -21,18 +18,102 @@ export default function ContactDisplay() {
   const linkedinTextureMeshRef = useRef<THREE.Mesh>(null!);
   const linkedinTexture = useTexture("textures/linkedin-logo.png");
 
+  const controls = useControls({
+    anisotropy: {
+      value: 0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    clearcoat: {
+      value: 0.0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    clearcoatRoughness: {
+      value: 0.0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    opacity: {
+      value: 0.8,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    color: "#2e1065",
+    emissiveColor: "#000000",
+    roughness: {
+      value: 0.1,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    metalness: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    ior: {
+      value: 1.1,
+      min: 1,
+      max: 2.66,
+      step: 0.01,
+    },
+    reflectivity: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    iridescence: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    iridescenceIOR: {
+      value: 1.3,
+      min: 1,
+      max: 2.66,
+      step: 0.01,
+    },
+    sheen: {
+      value: 0.0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    sheenRoughness: {
+      value: 1,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    sheenColor: "#000000",
+    specularIntensity: {
+      value: 1,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    specularColor: "#ffffff",
+  });
+
   useFrame(({ camera }, delta) => {
     if (groupRef.current) {
       groupRef.current.lookAt(camera.position);
     }
     if (githubOrbMeshRef.current) {
-      githubOrbMeshRef.current.rotation.x +=
-        delta * 0.1 * (Math.random() - 0.5);
-      githubOrbMeshRef.current.rotation.y += delta * 0.1;
+      githubOrbMeshRef.current.rotation.x += delta * 0.2 * Math.random();
+      githubOrbMeshRef.current.rotation.y += delta * 0.2 * Math.random();
     }
     if (linkedinOrbMeshRef.current) {
-      linkedinOrbMeshRef.current.rotation.x += delta * 0.1;
-      linkedinOrbMeshRef.current.rotation.y += delta * 0.1;
+      linkedinOrbMeshRef.current.rotation.x += delta * 0.2 * Math.random();
+      linkedinOrbMeshRef.current.rotation.y += delta * 0.2 * Math.random();
     }
     if (githubTextureMeshRef.current) {
       githubTextureMeshRef.current.lookAt(camera.position);
@@ -50,7 +131,26 @@ export default function ContactDisplay() {
         position={[-0.6, 1, 0]}
         renderOrder={1}
       >
-        <meshStandardMaterial color="#2e1065" roughness={0.2} metalness={0.5} />
+        <meshPhysicalMaterial
+          anisotropy={controls.anisotropy}
+          roughness={controls.roughness}
+          metalness={controls.metalness}
+          ior={controls.ior}
+          reflectivity={controls.reflectivity}
+          iridescence={controls.iridescence}
+          iridescenceIOR={controls.iridescenceIOR}
+          sheen={controls.sheen}
+          sheenRoughness={controls.sheenRoughness}
+          sheenColor={controls.sheenColor}
+          clearcoat={controls.clearcoat}
+          clearcoatRoughness={controls.clearcoatRoughness}
+          specularIntensity={controls.specularIntensity}
+          specularColor={controls.specularColor}
+          color={controls.color}
+          transparent
+          opacity={controls.opacity}
+          flatShading
+        />
         <Decal
           ref={githubTextureMeshRef}
           debug={debug}
@@ -62,47 +162,54 @@ export default function ContactDisplay() {
           <meshBasicMaterial
             map={githubTexture}
             transparent
-            // side={THREE.DoubleSide}
             polygonOffset
             polygonOffsetFactor={-500}
+            opacity={0.9}
           />
         </Decal>
       </Icosahedron>
       <Icosahedron
         ref={linkedinOrbMeshRef}
-        args={[0.45, 0]}
+        args={[0.45, 1]}
         position={[0.6, 1, 0]}
-        renderOrder={2}
+        renderOrder={1}
       >
         <meshPhysicalMaterial
-          roughness={0.1}
-          // thickness={0.15}
-          ior={1.0}
-          flatShading
-          // color="#ede9fe"
-          color="#2e1065"
+          // anisotropy={controls.anisotropy}
+          roughness={controls.roughness}
+          metalness={controls.metalness}
+          // ior={controls.ior}
+          reflectivity={controls.reflectivity}
+          // iridescence={controls.iridescence}
+          // iridescenceIOR={controls.iridescenceIOR}
+          // sheen={controls.sheen}
+          // sheenRoughness={controls.sheenRoughness}
+          // sheenColor={controls.sheenColor}
+          clearcoat={controls.clearcoat}
+          clearcoatRoughness={controls.clearcoatRoughness}
+          // specularIntensity={controls.specularIntensity}
+          // specularColor={controls.specularColor}
+          color={controls.color}
           transparent
-          // opacity={0.5}
-          transmission={0.2}
+          opacity={controls.opacity}
+          flatShading
         />
-        <mesh
+        <Decal
           ref={linkedinTextureMeshRef}
-          position={[0, 0, 0.01]}
-          renderOrder={1}
+          debug={debug}
+          position={[0, 0, 0.3]}
+          rotation={[0, 0, 0]}
+          scale={0.45}
+          renderOrder={2}
         >
-          <planeGeometry args={[0.4, 0.4]} />
-          <meshPhysicalMaterial
+          <meshBasicMaterial
             map={linkedinTexture}
             transparent
-            side={THREE.DoubleSide}
-            transmission={1}
+            polygonOffset
+            polygonOffsetFactor={-500}
+            opacity={0.9}
           />
-          {/* <meshBasicMaterial
-            map={linkedinTexture}
-            transparent
-            side={THREE.DoubleSide}
-          /> */}
-        </mesh>
+        </Decal>
       </Icosahedron>
     </group>
   );
